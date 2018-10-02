eb50ffce648e1ba86b1a6b279b8ceec67b3dc15d
coreycosman <cosman.corey@gmail.com>
2018-10-02 22:52:11 +0400

    add rooms, pull values from query string to dynamically indicate display name and room name, add room join and room leave functionality, display messagesto specific rooms only

--- server/server.js
+++ server/server.js
@@ -19,6 +19,7 @@
     const server = http.createServer(app);
     const io = socketIO(server);
     const {generateMessage, generateLocationMessage} = require('./utils/message');
+    const {isRealString } = require('./utils/validation');
 
 // CONTROLLER CONFIG:
     todosController(app);
@@ -78,31 +79,57 @@
 
     // ___________________________
 
-    // SERVER EVENT EMITTERS
-
-    socket.emit('newMessage', generateMessage('admin', 'welcome!'))
+    // SERVER EVENT LISTENERS:
 
     // ___________________________
 
-    // SERVER BROADCASTS
-
-    socket.broadcast.emit('newUserJoin', generateMessage('admin', 'new user joined'))
-
-    // ___________________________
-
-    // SERVER EVENT LISTENERS
+    // MESSAGE LISTENER
 
     socket.on('createMessage', (message, callback) => {
       console.log('createMessage', message);
-      io.emit('newMessage', generateMessage(message.from, message.text));
+      io.to(message.room).emit('newMessage', generateMessage(message.from, message.text));
       callback(`server and other clients received message: ${message.text}`);
     });
 
-    socket.on('createLocationMessage', (coords) => {
+    // ___________________________
 
-      socket.broadcast.emit('newLocationMessage', generateLocationMessage('admin', coords.latitude, coords.longitude));
+    // LOCATION BUTTON LISTENER
+
+    socket.on('createLocationMessage', (message, callback) => {
+
+      socket.broadcast.to(message.room).emit('newLocationMessage', generateLocationMessage('admin', message.latitude, message.longitude));
+      callback(`server and other clients received location: ${message.text}`);
     })
+
+    // ___________________________
+
+    // ROOM JOIN
+
+    socket.on('join', (params, callback) => {
+      if (!isRealString(params['display-name']) || !isRealString(params['room-name'])) {
+        callback('display name and room name are required');
+      }
+      socket.join(params['room-name']);
+      socket.emit('newMessage', generateMessage('admin', `hello ${params['display-name']}, welcome to the ${params['room-name']} chatroom!`))
+
+      socket.broadcast.to(params['room-name']).emit('userInOut', generateMessage('admin', `${params['display-name']} joined the chatroom`));
+      callback();
+    })
+
+    // ___________________________
+
+    // ROOM LEAVE
+
+    socket.on('leaveRoom', (params) => {
+      console.log(`${params['display-name']} left the ${params['room-name']} chatroom`)
+
+      socket.leave(params['room-name'])
+      socket.broadcast.to(params['room-name']).emit('userInOut', generateMessage('admin', `${params['display-name']} has left this chatroom`));
+    });
   });
+
+
+
 
   // server log
   // app.use((req, res, next) => {
