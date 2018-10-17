4c973a88bfbbc3ef4cf4a7f06ca55c8d9cdd96b6
coreycosman <cosman.corey@gmail.com>
2018-10-17 20:09:14 +0400

    sass config

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import Header from "./header";
-import Dashboard from "./dashboard";
-import Landing from "./landing";
+import Header from './header';
+import Dashboard from './dashboard';
+import Landing from './landing';
+import  '../sass/css-loader.scss';
 
 const App = () => {
   return (
